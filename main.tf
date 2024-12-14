5a5b54d01c19fcdda25ecc69d86e7366c1721ad5
bitsbuddy <48157073+bitsbuddy@users.noreply.github.com>
2024-12-14 15:59:41 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -69,12 +69,23 @@ resource "aws_instance" "my_instance" {
 
 # Create an S3 Bucket
 resource "aws_s3_bucket" "test_bucket" {
-  bucket = "test-rishi-xxxxx"  # Name of the S3 bucket (must be globally unique)
-  acl    = "private"  # Access control list
-
+  bucket = "test-rishi-xxxxx"  # Name of the S3 bucket (must be globally unique
   tags = {
     Name = "test-bucket"
   }
+}
+
+resource "aws_s3_bucket_ownership_controls" "test_bucket_ownership" {
+  bucket = aws_s3_bucket.test_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "test_bucket_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.test_bucket_ownership]
+  bucket = aws_s3_bucket.test_bucket.id
+  acl    = "private"
 }
 
 # Outputs
