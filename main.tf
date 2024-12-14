6f8e6388747c5ae4ab2090a78139b704a771cb9d
bitsbuddy <48157073+bitsbuddy@users.noreply.github.com>
2024-12-14 15:47:59 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -67,6 +67,12 @@ resource "aws_instance" "my_instance" {
   key_name = "my-key-pair"  # Replace with your existing key pair name
 }
 
+# Create an S3 Bucket
+resource "aws_s3_bucket" "test1" {
+  bucket = "test1"
+  acl    = "private"
+}
+
 # Outputs
 output "instance_public_ip" {
   value = aws_instance.my_instance.public_ip
